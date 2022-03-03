6b2ecb4113593bdfd44ae0820118394b9592d6d5
Unai <unaimartinezurraca@hotmail.com>
2022-03-03 22:44:22 +0100

    ejercicio3

--- app/src/main/java/com/example/ejercicioskotlin/MainActivity.kt
+++ app/src/main/java/com/example/ejercicioskotlin/MainActivity.kt
@@ -2,16 +2,44 @@ package com.example.ejercicioskotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ejercicioskotlin.databinding.ActivityMainBinding
+import java.text.NumberFormat
 import kotlin.math.PI
 import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        main();
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener { calculateTip() }
     }
+    fun calculateTip() {
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null) {
+            binding.tipResult.text = ""
+            return
+        }
 
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        var tip = tipPercentage * cost
+        if (binding.roundUpSwitch.isChecked) {
+            tip = kotlin.math.ceil(tip)
+        }
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+    }
 
     fun main() {
         val squareCabin = SquareCabin(6, 50.0)
@@ -118,4 +146,3 @@ class MainActivity : AppCompatActivity() {
             return super.floorArea() * floors
         }
     }
-}
